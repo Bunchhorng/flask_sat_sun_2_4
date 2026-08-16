dd004b6651fd75cc1d921979c0dd25cbd3157c02
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-08-16 17:08:14 +0700

    create product with image

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from connection import connect_db
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
@@ -79,6 +81,75 @@ def category_delete(id):
     cursor.close()
     db.close()
     return redirect(url_for('category_index'))
+
+
+# product route
+@app.route('/product/')
+def index_product():
+    return render_template('product/index.html')
+
+
+UPLOAD_FOLDER = "uploads/products"
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = {
+    "png",
+    "jpg",
+    "jpeg",
+    "webp"
+}
+
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower()
+        in ALLOWED_EXTENSIONS
+    )
+
+@app.route('/product/create', methods=['GET', 'POST'])
+def create_product():
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM categories")
+    categories = cursor.fetchall()
+
+    if request.method=="POST":
+        name = request.form['productName']
+        price = request.form['productPrice']
+        qty = request.form['productQty']
+        category_id = request.form['category_id']
+        
+        image = request.files.get('image')
+
+        image_name = None
+
+        if image and image.filename:
+
+            if allowed_file(image.filename):
+
+                image_name = secure_filename(image.filename)
+
+                os.makedirs(
+                    app.config["UPLOAD_FOLDER"],
+                    exist_ok=True
+                )
+
+                image.save(
+                    os.path.join(
+                        app.config["UPLOAD_FOLDER"],
+                        image_name
+                    )
+                )
+
+        cursor.execute('INSERT INTO products(name, price, quantity, category_id, image)' \
+        'VALUES(%s, %s, %s, %s, %s)',(name, price, qty, category_id, image_name))
+
+        db.commit()
+        cursor.close()
+        db.close()
+        return redirect(url_for('index_product'))
+    return render_template('product/create.html', categories=categories)
 
 if __name__=="__main__":
     app.run(debug=True)
